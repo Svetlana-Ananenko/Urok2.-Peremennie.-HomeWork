9f9deebeb1d8880651d40733bfdcf177f97d39ea
Zver <SvetlanaEvcheko@yandex.ru>
2024-05-07 16:55:14 +0300

    six tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,96 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int nameOne = 574;
+        byte nameTwo = 107;
+        short nameThree = 250;
+        long nameFour = 100000L;
+        float nameFive = 0.1234567f;
+        double nameSix = 0.1234567891011121;
+        System.out.println("Значение переменной 'nameOne' с типом 'int' равно " + nameOne);
+
+        System.out.println("Задача 2");
+        float a = 27.12f;
+        long b = 987678965549L;
+        double c = 2.786;
+        short d = 569;
+        short f = -159;
+        int g = 27897;
+        byte h = 67;
+        System.out.println("Значение переменной 'a' с типом 'float' равно " + a);
+        System.out.println("Значение переменной 'b' с типом 'long' равно " + b);
+        System.out.println("Значение переменной 'c' с типом 'double' равно " + c);
+        System.out.println("Значение переменной 'd' с типом 'short' равно " + d);
+        System.out.println("Значение переменной 'f' с типом 'short' равно " + f);
+        System.out.println("Значение переменной 'g' с типом 'int' равно " + g);
+        System.out.println("Значение переменной 'h' с типом 'byte' равно " + h);
+
+        System.out.println("Задача 3");
+        byte classOne = 23;
+        byte classTwo = 27;
+        byte classThree = 30;
+        int students = classOne + classTwo + classThree;
+        System.out.println("Всего учеников равно " + students);
+
+        byte totalSheets = 120;
+        System.out.println("Всего листов бумаги " + totalSheets);
+
+        int sheetsInOneClass = totalSheets / 3;
+        System.out.println("На каждого ученика рассчитано " + sheetsInOneClass + " листов бумаги");
+
+        System.out.println("Задача 4");
+        byte productivityIn2Minutes = 16;
+        int productivityIn1Minutes = productivityIn2Minutes / 2;
+        int productivityIn20Minutes = productivityIn1Minutes * 20;
+        int productivityIn1Day = productivityIn1Minutes * 60 * 24;
+        int productivityIn3Day = productivityIn1Day * 3;
+        int productivityIn1Month = productivityIn1Day * 31;
+        System.out.println("За '20 минут' машина произвела " + productivityIn20Minutes + " штук бутылок");
+        System.out.println("За 'сутки' машина произвела " + productivityIn1Day + " штук бутылок");
+        System.out.println("За '3 дня' машина произвела " + productivityIn3Day + " штук бутылок");
+        System.out.println("За 'месяц' машина произвела " + productivityIn1Month + " штук бутылок");
+
+        System.out.println("Задача 5");
+        byte wite = 2;
+        byte brown = 4;
+        byte paint = 120;
+        int paintOn1Class = wite + brown;
+        System.out.println("На один класс необходимо " + paintOn1Class + " банок краски");
+        int totalClass = paint / paintOn1Class;
+        System.out.println("В школе " + totalClass + " классов");
+        int totalWite = wite * totalClass;
+        System.out.println("Вcего потребуется " + totalWite + " банок белой краски");
+        int totalBrown = brown * totalClass;
+        System.out.println("Вcего потребуется " + totalBrown + " банок коричневой краски");
+        System.out.println("В школе, где " + totalClass + " классов, нужно " + totalWite + " банок белой краски и " + totalBrown + " банок коричневой краски");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
